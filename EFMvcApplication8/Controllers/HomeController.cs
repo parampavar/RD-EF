60830f10e38dd1ccb0773a25800d2158a7e1e0f4
Param Pavar <github@parampavar.com>
2013-07-01 12:46:04 -0700

    Hi

---
+++ EFMvcApplication8/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Config;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFMvcApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            DbConfiguration.SetConfiguration(new MyDbConfig());
+            MBM_SOPIC_SPINE_521XEntities dbc = new MBM_SOPIC_SPINE_521XEntities();
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            try
+            {
+                var rs = dbc.ADM_AUTHORITY_TYPE.Where(x => x.ID > 1);
+                ViewBag.RS = rs;
+
+                var li = dbc.ADM_LIST.Where(x => x.ID > 1);
+
+                ViewBag.LIST = li;
+
+            }
+            catch (Exception e)
+            {
+                
+                throw;
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EFMvcApplication8/MyDbConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Config;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.SqlServer;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EFMvcApplication8
+{
+    public class MyDbConfig : DbConfiguration
+    {
+        public MyDbConfig()
+        {
+            AddExecutionStrategy(() => new SqlAzureExecutionStrategy());
+        }
+        //private IExecutionStrategy myDbExecutionStrategy()
+        //{
+        //    SqlAzureExecutionStrategy mydb = new SqlAzureExecutionStrategy();
+        //    return mydb;
+        //}
+    }
+}
