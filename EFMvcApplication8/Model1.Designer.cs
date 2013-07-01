78ca3391381179cf51156604166a1d2457bd6076
Param Pavar <github@parampavar.com>
2013-07-01 14:25:31 -0700

    designer changes

--- EFMvcApplication8/Model1.Designer.cs
+++ EFMvcApplication8/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Working\R&D\.NET\EFMvcApplication8\EFMvcApplication8\Model1.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Working\Github\RD-EF\EFMvcApplication8\Model1.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
